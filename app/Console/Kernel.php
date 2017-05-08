6b6f45667a5e68eca108a6a4ab0c46005f6a31fa
dongfanfan <13315478388@163.com>
2017-05-08 11:22:15 +0800

    submit all

---
+++ app/Console/Kernel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use DB;
+use Log;
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        //
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        // $schedule->command('inspire')
+        //          ->hourly();
+        $schedule->call(function () {
+            // DB::table('recent_users')->delete();
+            Log::debug('schedule test --- schedule running !!!');
+        })->everyMinute();
+
+    }
+
+    /**
+     * Register the Closure based commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
+    {
+        require base_path('routes/console.php');
+    }
+}
