8c6774efa56779c99c2e6c121ceb9aa002181e66
dongfanfan <dongfanfan@bingobox.com>
2017-10-20 15:24:46 +0800

    add

---
+++ app/Http/Controllers/LogController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/10/20
+ * Time: 15:00
+ */
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Cache;
+use Log;
+
+class LogController extends Controller{
+    public function log(Request $request){
+        $data = $request->input('data');
+        dd($data);
+    }
+}
