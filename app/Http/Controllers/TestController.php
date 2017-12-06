f31e42c28e5c06c60d0884506d83040bc0570ab6
dongfanfan <dongfanfan@bingobox.com>
2017-12-06 20:13:52 +0800

    branch_test1

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Log;
+use DB;
+
+class TestController extends Controller
+{
+    public function test(){
+        $result = DB::table('box_order')->pluck('order_no');
+        dd($result);
+
+        //branch_test1
+    }
+}
